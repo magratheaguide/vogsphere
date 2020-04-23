19d1e9fc14eb73d324f6c4a6cc9ec626ef69accc
daine <veralidaine.thewildmage@gmail.com>
2020-04-22 19:12:57 -0600

    extract fillInClaims() and compileClaimPost(), clean up spacing in template literals

--- claim-code-generator.js
+++ claim-code-generator.js
@@ -126,6 +126,101 @@ Purpose: Convert member-provided answers from the associated form into the code 
         }
     }
 
+    function fillInClaims() {
+        let faceClaim = 
+`<div class="claim-row">
+    <span class="detail-alitus"><b>${input.faceClaim.value}</b></span> as 
+    <span class="detail-alitus no-bg text-color-${input.memberGroup.value}">
+        <a href="${input.profileUrl.value}" title="played by ${input.writerAlias.value}">${input.characterName.value}</a>
+    </span>
+</div>`;
+
+        let occupationClaim = 
+`<div class="list-item level-3">
+    <span class="list-taken-by text-color-${input.memberGroup.value}">
+        <a href="${input.profileUrl.value}">${input.characterName.value}</a>
+    </span> ${
+        input.occupation.value === ""
+        ? ""
+        : 
+    `<span class="list-aside">(${input.occupation.value})</span>`
+    }
+</div>`;
+
+        // labs are in the occupation claim list, so the occupation claim code is inserted into the lab claim
+        let labClaim = 
+`<div class="list-item level-1">
+    <span class="heading-dinorwic">${input.labName.value}</span>
+</div>
+
+<div class="textblock-aniak left list-item level-2">
+    ${input.labDescription.value}
+</div>
+
+<div class="list-item level-2">
+    <span class="heading-dollfus">Lead</span>
+    <span class="pill-gusev">Limit 1</span>
+</div>
+
+${input.isLabLead ? occupationClaim : ""}
+
+<div class="list-item level-2">
+    <span class="heading-dollfus">Staff</span>
+</div>
+
+${input.isLabLead ? "" : occupationClaim}`;
+
+        return {
+            faceClaim: faceClaim
+            , occupationClaim: occupationClaim
+            , labClaim: labClaim
+        }
+    }
+
+    function compileClaimPost(claims) {
+        let faceClaim = claims.faceClaim;
+        let occupationClaim = claims.occupationClaim;
+        let labClaim = claims.labClaim;
+
+        let code = 
+`${postBbcodeOpen}
+Face claim: 
+${codeBbcodeOpen} ${faceClaim} ${codeBbcodeClose}
+    
+Occupation claim: ${
+    input.memberGroup.value == "scientist"
+    ? 
+`
+Add to ${input.labName.value} as ${input.isLabLead ? "Lead" : "Staff"}`
+    : ""
+}
+${codeBbcodeOpen} ${input.isNewLab ? labClaim : occupationClaim} ${codeBbcodeClose} ${
+    input.requester.value || input.requestLocation.value
+    ? `
+
+${bold("REQUESTED CHARACTER")} ${
+        input.requester.value
+        ? `
+Requested by: ${input.requester.value}`
+        : ""
+    } ${
+        input.requestLocation.value
+        ? `
+Request location: ${
+            input.requestLocation.value 
+            && /^http/.test(input.requestLocation.value)
+            ? url(input.requestLocation.value)
+            : input.requestLocation.value
+        }`
+        : ""
+    }`
+    : ""
+} 
+${postBbcodeClose}`;
+
+        return code;
+    }
+
     function generateClaimCode() {
         reset();
 
@@ -140,93 +235,14 @@ Purpose: Convert member-provided answers from the associated form into the code 
         }
 
         console.log(input);
+ 
+        let claims = fillInClaims();
 
-        // process claims
-        let faceClaim = `<div class="claim-row">
-    <span class="detail-alitus"><b>${input.faceClaim.value}</b></span> as 
-    <span class="detail-alitus no-bg text-color-${input.memberGroup.value}"><a href="${input.profileUrl.value}" title="played by ${input.writerAlias.value}">${input.characterName.value}</a></span>
-</div>`;
+        console.log(claims.faceClaim);
+        console.log(claims.occupationClaim);
+        console.log(claims.labClaim);
 
-        let occupationClaim = `<div class="list-item level-3">
-    <span class="list-taken-by text-color-${
-        input.memberGroup.value
-    }"><a href="${input.profileUrl.value}">${
-        input.characterName.value
-    }</a></span>${
-        input.occupation.value === ""
-            ? ""
-            : `
-    <span class="list-aside">(${input.occupation.value})</span>`
-    }
-</div>`;
-
-        let labClaim;
-
-        // if adding a lab claim, then occupation claim needs to be inserted into lab claim
-        if (input.isNewLab) {
-            labClaim = `<div class="list-item level-1">
-        <span class="heading-dinorwic">${input.labName.value}</span>
-    </div>
-
-    <div class="textblock-aniak left list-item level-2">
-        ${input.labDescription.value}
-    </div>
-
-    <div class="list-item level-2">
-        <span class="heading-dollfus">Lead</span>
-        <span class="pill-gusev">Limit 1</span>
-    </div>
-
-    ${input.isLabLead ? occupationClaim : ""}
-
-    <div class="list-item level-2">
-        <span class="heading-dollfus">Staff</span>
-    </div>
-
-    ${input.isLabLead ? "" : occupationClaim}`;
-        }
-
-        console.log(faceClaim);
-        console.log(occupationClaim);
-        console.log(labClaim);
-
-        let code = `${postBbcodeOpen}
-    Face claim: 
-    ${codeBbcodeOpen} ${faceClaim} ${codeBbcodeClose}
-
-    Occupation claim: ${
-        input.memberGroup.value == "scientist"
-            ? `
-    Add to ${input.labName.value} as ${
-                  input.isLabLead ? "Lead" : "Staff"
-              }`
-            : ""
-    }
-    ${codeBbcodeOpen} ${
-            input.isNewLab ? labClaim : occupationClaim
-        } ${codeBbcodeClose}
-
-    ${
-        input.requester.value || input.requestLocation.value
-            ? `${bold("REQUESTED CHARACTER")}
-    ${
-        input.requester.value
-            ? `Requested by: ${input.requester.value}`
-            : ""
-    }`
-            : ""
-    } 
-    ${
-        input.requestLocation.value
-            ? `Request location: ${
-                  input.requestLocation.value &&
-                  /^http/.test(input.requestLocation.value)
-                      ? url(input.requestLocation.value)
-                      : input.requestLocation.value
-              }`
-            : ""
-    }
-    ${postBbcodeClose}`;
+        let code = compileClaimPost(claims);
 
         // put code in the code box for use
         resultBox.textContent = code;
